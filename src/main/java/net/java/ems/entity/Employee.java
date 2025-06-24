35d3be035fcee2628ed189e2f8e51390da50da0d
Shelvy <shelvy511@gmail.com>
2025-06-23 21:52:52 -0500

    added EmployeeDto and EmployeeMapper

--- src/main/java/net/java/ems/entity/Employee.java
+++ src/main/java/net/java/ems/entity/Employee.java
@@ -17,9 +17,9 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column( name = "first_name")
-    private String firstname;
+    private String firstName;
     @Column(name = "last_name")
-    private String lastname;
+    private String lastName;
     @Column(name = "email_id", nullable = false, unique = true)
     private String email;
 }
---
+++ src/main/java/net/java/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,15 @@
+package net.java.ems.mapper;
+
+import net.java.ems.dto.EmployeeDto;
+import net.java.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getEmail());
+    }
+
+    public static  Employee mapToEmployeeDto(EmployeeDto employeeDto) {
+        return new Employee(employeeDto.getId(), employeeDto.getFirstName(), employeeDto.getLastName(), employeeDto.getEmail());
+    }
+}
